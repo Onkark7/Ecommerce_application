b25be8470acecdeb0f631027eec4197d88d7f770
Onkark7 <106863878+Onkark7@users.noreply.github.com>
2022-07-10 07:15:45 +0530

    Add files via upload

---
+++ kdev/ecom/api/category/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+import email
+from django.db import migrations
+from api.user.models import CustomUser
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user= CustomUser(name="onkar",
+                         email="kulkarnio5470@gmail.com",
+                         is_staff=True,
+                         is_superuser="True",
+                         phone="9370213128",
+                         gender="Male")
+    
+        user.set_password("12345")
+        user. save()
+    
+
+
+    dependencies =[]
+
+
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
